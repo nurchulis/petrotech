78b0817fa30feea52d6ba27fc4418f5d8966879e
kang_edi <edi.zuviyanto@gmail.com>
2026-03-25 11:38:44 +0700

    pull view user 5

--- routes/web.php
+++ routes/web.php
@@ -75,6 +75,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
                 // VM Management (CRUD)
                 Route::resource('vm-management', VmManagementController::class)
                     ->parameters(['vm-management' => 'vm']);
+
+                Route::resource('users', UserController::class);
+
+                // VM Management (CRUD)
+                Route::resource('vm-management', VmManagementController::class)
+                    ->parameters(['vm-management' => 'vm']);
+
             }
             );
 
