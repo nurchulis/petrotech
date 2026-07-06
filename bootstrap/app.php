3413021fde98e5c104996d927c95d33a162da06f
nurchulissss <nurchuliss@gmail.com>
2026-07-07 01:36:24 +0700

    handle https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO);
+
         $middleware->append(\App\Http\Middleware\CheckMaintenanceMode::class);
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
 
